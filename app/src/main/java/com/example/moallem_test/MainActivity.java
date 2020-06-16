059693d69043b750e0624badc9ba2bf167d0dfca
hebamosafa <36491097+hebamosafa@users.noreply.github.com>
2020-06-16 17:42:07 +0200

    Last Version

--- app/src/main/java/com/example/moallem_test/MainActivity.java
+++ app/src/main/java/com/example/moallem_test/MainActivity.java
@@ -7,44 +7,57 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
-     VideoView videoView ;
+public class MainActivity extends AppCompatActivity  {
+    StateBroadcastingVideoView videoView ;
      ImageButton mPlayButton;
-     VideoView videoView2 ;
+    StateBroadcastingVideoView videoView2 ;
      ImageButton mPlayButton2;
      Uri uri;
     android.widget.MediaController mediaController;
-    android.widget.MediaController mediaController2;
+    int stopPosition1=0;
+    int stopPosition2=0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        videoView =(VideoView)findViewById(R.id.videoView);
+        videoView =(StateBroadcastingVideoView)findViewById(R.id.videoView);
         mPlayButton = (ImageButton) findViewById(R.id.imageButton);
 
-        videoView2 =(VideoView)findViewById(R.id.videoView2);
+        videoView2 =(StateBroadcastingVideoView)findViewById(R.id.videoView2);
         mPlayButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        //the vedio path
         uri=Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.road_car_view);
 
         //Creating MediaController
         mediaController= new MediaController(this);
-        mediaController2= new MediaController(this);
-        videoView.setMediaController(mediaController);
-        videoView2.setMediaController(mediaController2);
+        //set listeners when paused to get the last position
 
 
-        test(videoView,mPlayButton);
-        test(videoView2,mPlayButton2);
+
+        Controll_vedio(videoView,mPlayButton,1);
+        Controll_vedio(videoView2,mPlayButton2,2);
 
     }
-    void test(final VideoView videoView,final ImageButton mPlayButton){
+
+    void Controll_vedio(final StateBroadcastingVideoView videoView, final ImageButton mPlayButton,  final int id){
+        videoView.setPlayPauseListener(new StateBroadcastingVideoView.PlayPauseListener() {
+            @Override
+            public void onPause() {
+                mPlayButton.setVisibility(View.VISIBLE);
+                if(id==1)
+                {stopPosition1 = videoView.getCurrentPosition(); }
+                else { stopPosition2 = videoView.getCurrentPosition(); }
+            }
+        });
+
         mPlayButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -53,8 +66,12 @@ public class MainActivity extends AppCompatActivity {
                     videoView.pause();
                     mPlayButton.setVisibility(View.VISIBLE);
                 } else {
+                    videoView.setMediaController(mediaController);
                     videoView.setVideoURI(uri);
                     videoView.requestFocus();
+                    if(id==1)
+                    {videoView.seekTo(stopPosition1);}
+                    else {videoView.seekTo(stopPosition2); }
                     videoView.start();
                     mPlayButton.setVisibility(View.INVISIBLE);
                 }
@@ -62,5 +79,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
 
 }
